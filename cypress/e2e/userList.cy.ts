259f9d66ecabe8a0eead21b130420afc3270da0e
Ian Welerson <ian.nawes@gmail.com>
2022-09-05 22:09:20 -0300

    test: update E2E tests

--- cypress/e2e/userList.cy.ts
+++ cypress/e2e/userList.cy.ts
@@ -1,14 +1,86 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Acessing user list', () => {
-  it('can view a user list', () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080)
     cy.visit('/users')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users/states',
+      },
+      { fixture: '/users/states.json' }
+    ).as('getStateList')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users',
+      },
+      { fixture: '/users/list.json' }
+    ).as('getUsersDefault')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users?sort=name',
+      },
+      { fixture: '/users/listSortedByName.json' }
+    ).as('getUsersSortedByName')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users?sort=city',
+      },
+      { fixture: '/users/listSortedByCity.json' }
+    ).as('getUsersSortedByCity')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users?state=acre&sort=name',
+      },
+      { fixture: '/users/listFilteredByState.json' }
+    ).as('getUsersFilteredByState')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/v1/users?query=du&sort=name',
+      },
+      { fixture: '/users/listFilteredByName.json' }
+    ).as('getUsersFilteredByName')
+  })
+
+  it('can view a user list', () => {
     cy.contains('h1', 'Lista de membros')
-    cy.get('[data-testid=user-card]').should('have.length', 5)
+    cy.get('[data-testid=user-card]').should('have.length', 9)
+  })
+
+  it('can filter users by state', () => {
+    cy.get('[data-testid=state-filter-input]').first().click()
+
+    cy.wait('@getUsersFilteredByState')
+    cy.contains('p', 'acre')
+  })
+
+  it('can filter users by search bar', () => {
+    cy.get('[data-testid=search-bar-input]').type('du')
+
+    cy.wait('@getUsersFilteredByName')
+    cy.contains('p', 'alexandra duarte')
+  })
+
+  it('can filter users by search bar', () => {
+    cy.get('.base-select').select('city')
+
+    cy.wait('@getUsersSortedByCity')
+    cy.contains('p', 'denice ribeiro')
   })
 
   it('can access a user profile and get back by clicking on header logo', () => {
-    cy.visit('/users')
     cy.get('[data-testid=user-card]').first().click()
 
     cy.contains('p', 'alejandra rodrigues')
